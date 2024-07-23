8290eacbc3b9c9bd361369bb2beb3125e9bf19c2
anhyunji494 <anhyunji494@naver.com>
2024-07-23 15:00:50 +0900

    dictionaryRouter.js

--- server/src/public/js/user.js
+++ server/src/public/js/user.js
@@ -1,5 +1,21 @@
 // user.js
 
+const { default: axios } = require("axios");
+const { response } = require("express");
+
 const handleLogout = () => {
   // logout btn ck
+  console.log("logout btn ck");
+
+  axios.post("/user/logout").then((res) => {
+    if (response.data.success) {
+      console.log(window.history);
+      localStorage.clear();
+      console.log(localStorage.getItem("userEmail"));
+      window.location.href("/main");
+    } else {
+      alert("로그아웃 실패!");
+      console.log(localStorage.getItem("userEmail"));
+    }
+  });
 };
