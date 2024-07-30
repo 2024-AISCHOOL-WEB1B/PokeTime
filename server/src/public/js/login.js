4f0190f25c73c55d5d4fb614aba98521e93cf44d
anhyunji494 <anhyunji494@naver.com>
2024-07-30 15:53:24 +0900

    port setting

--- server/src/public/js/login.js
+++ server/src/public/js/login.js
@@ -39,7 +39,7 @@ const handleLogin = () => {
 
   // axios 통신
   axios
-    .post("http://localhost:3000/user/login", {
+    .post("http://54.180.231.199:3000/user/login", {
       userEmail,
       userPw,
     })
