464af5296e4d2e7c8cf63b74c1e90f494b94ed15
ummykk0210 <ummykk0210@gmail.com>
2024-07-30 11:36:50 +0900

    메인페이지 알림창 추가 / 관리페이지 캘린더 수정

--- server/src/public/js/manage.js
+++ server/src/public/js/manage.js
@@ -19,72 +19,13 @@ document.addEventListener("DOMContentLoaded", () => {
   let uploadFile = null;
   let stream;
 
-  let today = new Date();
-  let currentMonth = today.getMonth();
-  let currentYear = today.getFullYear();
-
-  function renderCalendar(month, year) {
-    calendarBody.innerHTML = "";
-    const firstDay = new Date(year, month).getDay();
-    const daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-    calendarTitle.textContent = `${year}년 ${month + 1}월`;
-
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
-      const row = document.createElement("tr");
-      for (let j = 0; j < 7; j++) {
-        if (i === 0 && j < firstDay) {
-          const cell = document.createElement("td");
-          const cellText = document.createTextNode("");
-          cell.appendChild(cellText);
-          row.appendChild(cell);
-        } else if (date > daysInMonth) {
-          break;
-        } else {
-          const cell = document.createElement("td");
-          const cellText = document.createTextNode(date);
-          cell.appendChild(cellText);
-
-          if (
-            date === today.getDate() &&
-            year === today.getFullYear() &&
-            month === today.getMonth()
-          ) {
-            cell.classList.add("today");
-          }
-
-          if (j === 0 || j === 6) {
-            cell.classList.add("weekend");
-          }
-
-          row.appendChild(cell);
-          date++;
-        }
-      }
-      calendarBody.appendChild(row);
-    }
-  }
-
-  prevMonth.addEventListener("click", () => {
-    currentMonth--;
-    if (currentMonth < 0) {
-      currentMonth = 11;
-      currentYear--;
-    }
-    renderCalendar(currentMonth, currentYear);
-  });
-
-  nextMonth.addEventListener("click", () => {
-    currentMonth++;
-    if (currentMonth > 11) {
-      currentMonth = 0;
-      currentYear++;
-    }
-    renderCalendar(currentMonth, currentYear);
-  });
-
-  renderCalendar(currentMonth, currentYear);
+     // const TodoListElement = document.getElementById('todo-list');
+    // if(data.length === 0){
+    //     TodoListElement.textContent = '오늘 해야 할 일을 등록해주세요 !';
+    // }
+    // else{
+    //     TodoListElement.innerHTML =
+    // }
 
   if (camera) {
     camera.onclick = function () {
