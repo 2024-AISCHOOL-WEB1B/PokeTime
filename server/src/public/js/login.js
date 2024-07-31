d064536744f9eb2c379752b6b5441732ad7f01e9
anhyunji494 <anhyunji494@naver.com>
2024-07-31 15:38:40 +0900

    host modify

--- server/src/public/js/login.js
+++ server/src/public/js/login.js
@@ -1,36 +1,36 @@
 // login.js
 
 // inputEmail input 변수
-let inputEmail = document.querySelector("#login_email");
+let inputEmail = document.querySelector('#login_email');
 // inputPassword 변수
-let inputPw = document.querySelector("#login_password");
+let inputPw = document.querySelector('#login_password');
 
 // email 유효성 검사를 위한 패턴
 const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
 
 // 로그인 버튼 클릭
 const handleLogin = async () => {
-  console.log("login btn ck");
+  console.log('login btn ck');
 
   // 이메일 유효성 검사
 
-  const ckId = document.getElementById("result_login_email");
-  const ckPw = document.getElementById("result_login_password");
+  const ckId = document.getElementById('result_login_email');
+  const ckPw = document.getElementById('result_login_password');
   const userEmail = inputEmail.value;
   const userPw = inputPw.value;
 
   // 값 확인
-  console.log("userEmail: ", userEmail);
-  console.log("userPw: ", userPw);
+  console.log('userEmail: ', userEmail);
+  console.log('userPw: ', userPw);
 
-  if (userEmail === "") {
-    ckId.innerHTML = "올바른 형식의 이메일을 입력해주세요.";
+  if (userEmail === '') {
+    ckId.innerHTML = '올바른 형식의 이메일을 입력해주세요.';
   } else if (pattern.test(userEmail) === false) {
-    ckId.innerHTML = "올바른 형식의 이메일을 입력해주세요.";
+    ckId.innerHTML = '올바른 형식의 이메일을 입력해주세요.';
   }
 
-  if (userPw === "") {
-    ckPw.innerHTML = "올바른 형식의 비밀번호를 입력해주세요.";
+  if (userPw === '') {
+    ckPw.innerHTML = '올바른 형식의 비밀번호를 입력해주세요.';
   }
 
   // // localStorage에 id, pw 키값 저장
@@ -39,24 +39,24 @@ const handleLogin = async () => {
 
   // axios 통신
   await axios
-    .post("http://localhost:3000/user/login", {
+    .post('http://http://54.180.231.199:3000/user/login', {
       userEmail,
       userPw,
     })
     .then((res) => {
       console.log(res);
-      if (res.data.result == "로그인실패") {
-        alert("이메일 또는 비밀번호를 확인하세요.");
-      } else if (res.data.result == "로그인성공") {
-        console.log("로그인 성공");
-        sessionStorage.setItem("userEmail", userEmail);
-        const checkinres = axios.post("/point/attend");
+      if (res.data.result == '로그인실패') {
+        alert('이메일 또는 비밀번호를 확인하세요.');
+      } else if (res.data.result == '로그인성공') {
+        console.log('로그인 성공');
+        sessionStorage.setItem('userEmail', userEmail);
+        const checkinres = axios.post('/point/attend');
         console.log(checkinres.data);
         // 로그인 성공 시 메인 페이지로 리다이렉션
-        window.location.href = "/mainpage";
+        window.location.href = '/mainpage';
       }
     })
     .catch((error) => {
-      console.error("로그인 요청 실패");
+      console.error('로그인 요청 실패');
     });
 };
--- server/src/public/js/manage.js
+++ server/src/public/js/manage.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  const calendarTitle = document.getElementById("calendarTitle");
-  const calendarBody = document.getElementById("calendarBody");
+document.addEventListener('DOMContentLoaded', async () => {
+  const calendarTitle = document.getElementById('calendarTitle');
+  const calendarBody = document.getElementById('calendarBody');
 
-  const prevMonth = document.getElementById("prevMonth");
-  const nextMonth = document.getElementById("nextMonth");
+  const prevMonth = document.getElementById('prevMonth');
+  const nextMonth = document.getElementById('nextMonth');
 
-  const modal = document.getElementById("modal");
-  const modalText = document.getElementById("modalText");
-  const closeModal = document.getElementsByClassName("close")[0];
-  const camera = document.getElementById("left_box");
-  const camera_upload = document.getElementById("camera_upload");
-  const camera_capture = document.getElementById("camera_capture");
-  const file_upload_btn = document.getElementById("file_upload");
-  const file_choose = document.getElementById("file_choose");
-  const preview = document.getElementById("preview");
-  const predict_result = document.getElementById("predict_result");
-  const attend_date = document.getElementById("attend_date");
+  const modal = document.getElementById('modal');
+  const modalText = document.getElementById('modalText');
+  const closeModal = document.getElementsByClassName('close')[0];
+  const camera = document.getElementById('left_box');
+  const camera_upload = document.getElementById('camera_upload');
+  const camera_capture = document.getElementById('camera_capture');
+  const file_upload_btn = document.getElementById('file_upload');
+  const file_choose = document.getElementById('file_choose');
+  const preview = document.getElementById('preview');
+  const predict_result = document.getElementById('predict_result');
+  const attend_date = document.getElementById('attend_date');
 
-  const todoList = document.getElementById("todo-list");
+  const todoList = document.getElementById('todo-list');
 
   let uploadFile = null;
   let stream;
 
-  const res = await axios.get("/user/attendcount");
+  const res = await axios.get('/user/attendcount');
 
   console.log(res.data.rows);
   attend_date.textContent = `${res.data.rows[0].attend_cnt}일`;
@@ -37,46 +37,46 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const searchTodo = async () => {
     try {
-      const res = await axios.get("/user/searchscheduler");
+      const res = await axios.get('/user/searchscheduler');
       console.log(res.data);
       if (res.data.rows.length === 0) {
-        todoList.textContent = "오늘 해야 할 일을 등록해주세요 !";
+        todoList.textContent = '오늘 해야 할 일을 등록해주세요 !';
       } else if (res.data.rows.length > 0) {
-        todoList.textContent = "";
+        todoList.textContent = '';
         res.data.rows.forEach((todo) => {
-          const listItem = document.createElement("li");
+          const listItem = document.createElement('li');
           listItem.innerHTML += `${todo.schedule_name} `;
           todoList.appendChild(listItem);
           // console.log(todo.schedule_name);
         });
       }
     } catch (error) {
-      console.error("Error fetching user data:", error);
+      console.error('Error fetching user data:', error);
     }
   };
 
   if (camera) {
     camera.onclick = function () {
-      modal.style.display = "block";
+      modal.style.display = 'block';
     };
   }
 
   closeModal.onclick = function () {
-    modal.style.display = "none";
+    modal.style.display = 'none';
 
     if (stream) {
       stream.getTracks().forEach((track) => track.stop());
-      camera_preview.innerHTML = "";
+      camera_preview.innerHTML = '';
     }
   };
 
   window.onclick = function (event) {
     if (event.target == modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none';
 
       if (stream) {
         stream.getTracks().forEach((track) => track.stop());
-        camera_preview.innerHTML = "";
+        camera_preview.innerHTML = '';
       }
     }
   };
@@ -84,17 +84,17 @@ document.addEventListener("DOMContentLoaded", async () => {
   // 모달창 내 사진등록 (파일 업로드)
 
   // 사진파일 올리기 버튼 클릭 시 숨겨진 file_choose input태그를 클릭한다.
-  file_upload_btn.addEventListener("click", function () {
+  file_upload_btn.addEventListener('click', function () {
     file_choose.click();
   });
 
-  file_choose.addEventListener("change", function (event) {
+  file_choose.addEventListener('change', function (event) {
     const file = event.target.files[0];
 
     // 카메라 이용 중 파일등록 버튼 클릭 시 카메라 종료 및 카메라 화면영역 삭제
     if (stream) {
       stream.getTracks().forEach((track) => track.stop());
-      camera_preview.innerHTML = "";
+      camera_preview.innerHTML = '';
     }
 
     // 업로드 파일 변수에 저장
@@ -105,12 +105,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       reader.onload = function (e) {
         // 파일 미리보기
-        const img = document.createElement("img");
+        const img = document.createElement('img');
 
         img.src = e.target.result;
-        img.style.maxWidth = "90px";
-        img.style.maxHeight = "90px";
-        preview.innerHTML = "";
+        img.style.maxWidth = '90px';
+        img.style.maxHeight = '90px';
+        preview.innerHTML = '';
         preview.appendChild(img);
       };
       reader.readAsDataURL(file);
@@ -118,69 +118,69 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
   // 직접찍기 버튼 클릭 시 사용자의 카메라를 실행시킨다.
-  camera_upload.addEventListener("click", function () {
-    preview.innerHTML = "";
+  camera_upload.addEventListener('click', function () {
+    preview.innerHTML = '';
     navigator.mediaDevices
       .getUserMedia({ video: true })
       .then(function (mediaStream) {
         stream = mediaStream;
-        const video = document.createElement("video");
+        const video = document.createElement('video');
         video.srcObject = mediaStream;
         video.autoplay = true;
-        video.style.maxWidth = "100px";
-        video.style.maxHeight = "100px";
-        camera_preview.innerHTML = "";
+        video.style.maxWidth = '100px';
+        video.style.maxHeight = '100px';
+        camera_preview.innerHTML = '';
         camera_preview.appendChild(video);
 
-        const captureButton = document.createElement("button");
-        captureButton.textContent = "촬영하기";
-        captureButton.classList.add("capture-button");
-        captureButton.addEventListener("click", function () {
-          const canvas = document.createElement("canvas");
+        const captureButton = document.createElement('button');
+        captureButton.textContent = '촬영하기';
+        captureButton.classList.add('capture-button');
+        captureButton.addEventListener('click', function () {
+          const canvas = document.createElement('canvas');
           canvas.width = video.videoWidth;
           canvas.height = video.videoHeight;
-          const ctx = canvas.getContext("2d");
+          const ctx = canvas.getContext('2d');
           ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-          const img = document.createElement("img");
-          img.src = canvas.toDataURL("image/png");
-          img.style.maxWidth = "100px";
-          img.style.maxHeight = "100px";
-          preview.innerHTML = "";
+          const img = document.createElement('img');
+          img.src = canvas.toDataURL('image/png');
+          img.style.maxWidth = '100px';
+          img.style.maxHeight = '100px';
+          preview.innerHTML = '';
           preview.appendChild(img);
           stream.getTracks().forEach((track) => track.stop());
-          camera_preview.innerHTML = "";
+          camera_preview.innerHTML = '';
 
           // 캡처된 이미지를 파일로 저장
           canvas.toBlob(function (blob) {
-            uploadFile = new File([blob], "capture_img.png", {
-              type: "image/png",
+            uploadFile = new File([blob], 'capture_img.png', {
+              type: 'image/png',
             });
           });
         });
         camera_preview.appendChild(captureButton);
       })
       .catch(function (err) {
-        console.log("An error occurred: " + err);
+        console.log('An error occurred: ' + err);
       });
   });
 
-  if (!("URL" in window) && "webkitURL" in window) {
+  if (!('URL' in window) && 'webkitURL' in window) {
     window.URL = window.webkitURL;
   }
 
   // 서버 전송
-  img_submit.addEventListener("click", async () => {
+  img_submit.addEventListener('click', async () => {
     if (uploadFile) {
       const formData = new FormData();
-      formData.append("image", uploadFile);
+      formData.append('image', uploadFile);
 
       try {
         const res = await axios.post(
-          "http://localhost:5000/predict",
+          'https://3iwnosr4ib.execute-api.ap-northeast-2.amazonaws.com/dev3/predict',
           formData,
           {
             headers: {
-              "Content-Type": "multipart/form-data",
+              'Content-Type': 'multipart/form-data',
             },
           }
         );
@@ -189,27 +189,27 @@ document.addEventListener("DOMContentLoaded", async () => {
         if (res.data.confidence > 0.7) {
           try {
             const pointRes = await axios.post(
-              "http://localhost:3000/point/picture"
+              'http://http://54.180.231.199:3000/point/picture'
             );
             console.log(pointRes.data);
-            modalText.textContent = "포인트 10점 획득!";
+            modalText.textContent = '포인트 10점 획득!';
             predict_result.innerHTML = `예측 결과: ${res.data.top}`;
-            modal.style.display = "block";
+            modal.style.display = 'block';
           } catch (error) {
-            console.error("포인트 획득 요청 실패:", error);
-            modalText.textContent = "포인트 획득 실패!";
-            modal.style.display = "block";
+            console.error('포인트 획득 요청 실패:', error);
+            modalText.textContent = '포인트 획득 실패!';
+            modal.style.display = 'block';
           }
         } else {
-          console.log("포인트 획득 실패");
-          modalText.textContent = "포인트 획득 실패!";
+          console.log('포인트 획득 실패');
+          modalText.textContent = '포인트 획득 실패!';
           predict_result.innerHTML = `예측 결과: 먹을만한 음식이 아닙니다.`;
-          modal.style.display = "block";
+          modal.style.display = 'block';
         }
       } catch (error) {
-        console.error("이미지 예측 요청 실패:", error);
-        modalText.textContent = "이미지 예측 실패!";
-        modal.style.display = "block";
+        console.error('이미지 예측 요청 실패:', error);
+        modalText.textContent = '이미지 예측 실패!';
+        modal.style.display = 'block';
       }
     }
   });
